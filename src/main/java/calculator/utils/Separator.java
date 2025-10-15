dee975cfb9309117de4dd470f0741615a2efb6a8
A7024E <rudqls6671@naver.com>
2025-10-16 05:59:32 +0900

    feat: 문자열 구분자 분리 기능 추가 - 입력된 문자열을 쉼표(,) 또는 콜론(:) 구분자로 분리한다. - 커스텀 구분자는 문자열 앞부분의 "//"와 "\n" 사이에 위치하는 문자를 커스텀 구분자로 사용한다.

---
+++ src/main/java/calculator/utils/Separator.java
@@ -0,0 +1,54 @@
+package calculator.utils;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Separator {
+    private static final String CUSTOM_PATTERN = "^//(.*)\\\\n(.*)";
+    private static final String DEFAULT_PATTERN = "[,:]";
+
+    public static String[] splitByDelimiter(String input) {
+        Matcher matcher = compliMatcher(input);
+        if (matcher.find()) {
+            if (isFindMatcher(matcher)) {
+                String delimiter = findCustomDelimiter(matcher);
+                splitByCustomDelimiter(matcher, delimiter);
+            }
+        }
+        return splitByDefaultDelimiter(input);
+    }
+
+    private static String[] splitByDefaultDelimiter(String input) {
+        return input.split(DEFAULT_PATTERN);
+    }
+
+    private static String[] splitByCustomDelimiter(Matcher matcher, String delimiter) {
+        return findDelimitedValues(matcher).split(delimiter);
+    }
+
+    private static Matcher compliMatcher(String input) {
+        return compliePattern().matcher(input);
+    }
+
+    private static Pattern compliePattern() {
+        return Pattern.compile(CUSTOM_PATTERN);
+    }
+
+    private static String findDelimitedValues(Matcher matcher) {
+        return matcher.group(2);
+    }
+
+    private static String findCustomDelimiter(Matcher matcher) {
+        return matcher.group(1);
+    }
+
+    private static boolean isFindMatcher(Matcher matcher) {
+        return matcher.find();
+    }
+
+
+}
+
+
