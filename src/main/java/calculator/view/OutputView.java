13f80d7b0bb9d7dca27b5e55946d1533a6dba1c3
A7024E <rudqls6671@naver.com>
2025-10-16 22:07:18 +0900

    refactor: 불필요한 import 제거 및 공백 제거

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,7 +1,7 @@
 package calculator.view;
 
 public class OutputView {
-    public void printSumResult(int result){
-        System.out.println("결과 : "+result);
+    public void printSumResult(int result) {
+        System.out.println("결과 : " + result);
     }
 }
