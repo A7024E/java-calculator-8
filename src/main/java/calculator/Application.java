62c64d95fccc8fd572056423345aab2f600a5213
A7024E <rudqls6671@naver.com>
2025-10-16 22:04:08 +0900

    feat: 애플리케이션 실행 진입점(main) 구현
    - CalculateController를 생성하여 프로그램 실행 흐름을 시작하도록 구성
    - main 메서드에서 run() 호출을 통해 전체 계산 프로세스 실행
    - Application 클래스는 프로그램의 시작 역할을 담당

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,13 @@
 package calculator;
 
+import calculator.Controller.CalculateController;
+import calculator.view.InputView;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CalculateController  calculateController = new CalculateController();
+        calculateController.run();
+
     }
 }
