f19eabcc0d8a40352c12ada0c73ba7f4ce3dd116
A7024E <rudqls6671@naver.com>
2025-10-16 21:25:35 +0900

    feat: 구분자와 양수 사이에 공백 입력한 경우 예외발생 처리

--- src/main/java/calculator/utils/Calculator.java
+++ src/main/java/calculator/utils/Calculator.java
@@ -4,12 +4,25 @@ import calculator.model.PositiveNumber;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Calculator {
+    public static void main(String[] args) {
+        String input =Console.readLine();
+
+        int i = splitCalculator(input);
+        System.out.println(i);
+    }
 
     public static int splitCalculator(String input) {
         if(isNull(input) || isEmpty(input)){
             return 0;
         }
+        validateCalculator(input);
         return calculateSum(Separator.splitByDelimiter(input));
+    }
+
+    private static void validateCalculator(String input) {
+        if(input.contains(" ")){
+            throw new IllegalArgumentException("공백을 포함할 순 없습니다");
+        }
     }
 
     private static boolean isEmpty(String input) {
@@ -32,6 +45,5 @@ public class Calculator {
     private static int convertNumeric(String input) {
         return Integer.parseInt(input);
     }
-
 
 }
