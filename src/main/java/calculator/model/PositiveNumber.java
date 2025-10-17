fad4b381c5d376d01633e28bae22c8fbd932efc9
A7024E <rudqls6671@naver.com>
2025-10-17 18:01:35 +0900

    refactor: 양수 최소값을 상수로 추출하여 의미 명확화

--- src/main/java/calculator/model/PositiveNumber.java
+++ src/main/java/calculator/model/PositiveNumber.java
@@ -3,6 +3,7 @@ package calculator.model;
 public class PositiveNumber {
 
     private final int number;
+    private static final int MIN_POSITIVE_NUMBER = 1;
     private static final String INVALID_NEGATIVE_NUMBER_EXCEPTION_MESSAGE = "숫자는 양수만 입력 가능합니다";
 
     private PositiveNumber(int number) {
@@ -21,7 +22,7 @@ public class PositiveNumber {
     }
 
     private boolean isPositiveNumberRange(int number) {
-        return number < 1;
+        return number < MIN_POSITIVE_NUMBER;
     }
 
     public int sumNumbers(int sum) {
