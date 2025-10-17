20b31b854af031c29c12f285227eee02b125f254
A7024E <rudqls6671@naver.com>
2025-10-17 17:56:05 +0900

    refactor: 입력 메시지를 상수로 추출하여 일관성 확보

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -4,8 +4,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String INPUT_PROMPT_MESSAGE = "덧셈할 문자열을 입력해 주세요.";
+
     public String inputStrings() {
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        System.out.println(INPUT_PROMPT_MESSAGE);
         return Console.readLine();
     }
 }
