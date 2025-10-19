d6116025921027eae0b92ffdd237045e6096b74a
A7024E <rudqls6671@naver.com>
2025-10-19 17:54:20 +0900

    refactor: 계산의 범위를 초과하였습니다 예외메시지 상수화

--- src/main/java/calculator/model/PositiveNumber.java
+++ src/main/java/calculator/model/PositiveNumber.java
@@ -5,6 +5,7 @@ public class PositiveNumber {
     private final long number;
     private static final long MIN_POSITIVE_NUMBER = 1L;
     private static final String INVALID_NEGATIVE_NUMBER_EXCEPTION_MESSAGE = "숫자는 양수만 입력 가능합니다";
+    private static final String CALCULATION_OVERFLOW_ERROR_MESSAGE = "계산의 범위를 초과하였습니다.";
 
     private PositiveNumber(long number) {
         validatePositiveNumber(number);
@@ -32,7 +33,7 @@ public class PositiveNumber {
 
     private void validateOverFlow(long sum) {
         if (isAdditionOverflow(sum)) {
-            throw new IllegalArgumentException(" 계산의 범위를 초과하였습니다.");
+            throw new IllegalArgumentException(CALCULATION_OVERFLOW_ERROR_MESSAGE);
         }
     }
 
